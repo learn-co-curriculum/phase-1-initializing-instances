3a80b3bf105bc625fe6194819e1b5a0e65ec3626
Steven G. Harms <steven.harms@flatironschool.com>
2019-04-15 12:59:59 -0700

    Recommit solution

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food
+    this.drink = drink
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad
+    this.soup = soup
+    this.drink = drink
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad 
+    this.soup = soup 
+    this.entree = entree 
+    this.dessert = dessert
+  }
+}
